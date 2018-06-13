874a7340bd07bde26211d088b2d2d3e70265aa78
grandriamitiry@bocasay.com <grandriamitsiry@bocasay.com>
2018-06-13 15:52:38 +0300

    add agenttranslation formtype

---
+++ src/AppBundle/Form/AgentTranslationType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Locale;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AgentTranslationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('locale', EntityType::class, ['class'=>Locale::class, 'choice_label'=>'libelle'])
+            ->add('passionTitre')
+            ->add('passionContenu')
+            ->add('destinationTitre')
+            ->add('destinationContenu')
+            ->add('formationTitre')
+            ->add('formationContenu')
+            ->add('cursus')
+            ->add('cursusSolidaire')
+            ->add('signatureTel')
+            ->add('signatureHoraireTravail')
+            ->add('signatureVisuel')
+            ->add('phrase');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\AgentTranslation'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_agenttranslation';
+    }
+}
